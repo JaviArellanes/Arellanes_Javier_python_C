5d0b0ce7fdbabcd32c9ddaf6f0502bc938d5787e
JaviArellanes <javierarellanes66@gmail.com>
2023-06-06 21:24:22 -0600

    Nuevo proyecto push a GitHub

---
+++ DIAGNOSTICO/ej_002.py
@@ -0,0 +1,18 @@
+print("¿Cuál es tu nombre(s)?")
+nombre = input()
+print("¿Cuál es tu primer apellido?")
+apellido = input()
+print("¿Cuál es tu segundo apellido?")
+apellido2 = input()
+print("¿En qué año naciste?")
+anio = input()
+print("¿En qué mes y día naciste? (en el siguiente formato “mm-dd”)" )
+day_month = input()
+
+print('A. Este es tu nombre en mayúsculas: '+ nombre.upper())
+print('B. Este es tu nombre en minúsculas: '+ nombre.lower())
+print('Esta es tu fecha de nacimiento: ')
+print()
+
+edad = input()
+print(edad)
